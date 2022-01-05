2d7aaf75e187a94487568dc5e8c60e1ed3463ca3
Binaryleo <leomouracss@fastmail.com>
2022-01-04 23:50:45 -0300

    Notification for temp. above 27°c +  blink led and

--- pico_mainScript/Main.py
+++ pico_mainScript/Main.py
@@ -1,6 +1,8 @@
-import machine
+from machine import Pin
+import time
 import utime
- 
+
+led = Pin(25, Pin.OUT)
 sensor_temp = machine.ADC(4)
 conversion_factor = 3.3 / (65535)
  
@@ -9,3 +11,12 @@ while True:
     temperature = 27 - (reading - 0.706)/0.001721
     print ("{0:.1f}".format(temperature)) 
     utime.sleep(2)
+    if temperature >= 27:
+        print("check your cooling system .. temperature above the ideal")
+        led.value(1)
+        time.sleep(1)
+        led.value(0)
+        time.sleep(1)
+    else:
+         led.value(0)
+         time.sleep(0)
