547d204994daa80e11ee6613b0857d7fdc73cf2e
Binaryleo <leomouracss@fastmail.com>
2022-01-16 02:32:04 -0300

    readme update

--- python_version/index.py
+++ python_version/index.py
@@ -16,27 +16,26 @@ print(ListPorts)  # Print the available ports
 port = input("Type a COM port: ").upper()
 
 if port[-1].isdigit:
- port = "COM" + port
-print(port)
-if port in ListPorts:
-    print(port + " is available to read data, plz continue and ...")
-    baud_rate = input("Type the Baud Rate: ")
-    if baud_rate.isdigit(): # Baudrate validator
-       pass
-    else:
-       print("Baudrate invalid ... new baudrade: 115200")
-       baud_rate =115200  
-    ser = serial.Serial("{}".format(port), baud_rate) # Serial takes two parameters: serial device and baudrate
-   
-    if(ser.isOpen()):
-      print("This port is alredy open")
-      while True:
-        data = ser.readline()
-        data = data.replace(b'\n', b'').replace(b'\r', b'')  # format string
-        print("Current temperature: " + str(data, 'utf-8'))  # utf-8 string
-else:
-    print("Check the available ports and try again")
-    print(ListPorts)
+
+  if port in ListPorts:
+      print(port + " is available to read data, plz continue and ...")
+      baud_rate = input("Type the Baud Rate: ")
+      if baud_rate.isdigit(): # Baudrate validator
+        pass
+      else:
+        print("Baudrate invalid ... new baudrade: 115200")
+        baud_rate =115200  
+      ser = serial.Serial("{}".format(port), baud_rate) # Serial takes two parameters: serial device and baudrate
+    
+      if(ser.isOpen()):
+        print("This port is alredy open")
+        while True:
+          data = ser.readline()
+          data = data.replace(b'\n', b'').replace(b'\r', b'')  # format string
+          print("Current temperature: " + str(data, 'utf-8'))  # utf-8 string
+  else:
+      print("Check the available ports and try again")
+      print(ListPorts)
 
 
 
